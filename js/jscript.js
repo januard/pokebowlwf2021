0efe8a99812f6c7f14e576bae6ab9223edd26dfe
januard <paulsoterana@gmail.com>
2022-04-20 17:03:38 +0800

    good

---
+++ js/jscript.js
@@ -0,0 +1,5 @@
+const CONTACTCLICK = document.getElementById('contact-label');
+CONTACTCLICK.addEventListener('click', function(event) {
+    event.preventDefault();
+    alert('Call Us at 555-5555');
+});
